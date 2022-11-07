fc1c54e79743f96c78fa66b3c324b0310e21b255
Vinicius Santos <viniciusantos0898@gmail.com>
2022-11-07 18:25:58 -0300

    Bugs menores removidos; Finalizada a tela de seleção de personagem

--- Assets/Scripts/Menus & UI/SelectionScreen/HighlightChar.cs
+++ Assets/Scripts/Menus & UI/SelectionScreen/HighlightChar.cs
@@ -9,10 +9,6 @@ public class HighlightChar : MonoBehaviour
     [SerializeField] private RectTransform highlightP2;
 
 
-    private void Start()
-    {
-    }
-
     private void Update()
     {
         if (selectCharacterScreen.Count.Equals(2))
